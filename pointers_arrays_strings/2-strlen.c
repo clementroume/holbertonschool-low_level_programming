0861505616ae48722ef3bb1266ef339983fa3460
clementroume <clement.roume@hotmail.com>
2024-11-21 16:29:24 +0100

    2. This report, by its very length

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * _strlen - function return the length of a string
+ * @s: The string
+ * Return: String length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
