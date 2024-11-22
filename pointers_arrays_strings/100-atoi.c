ad39ccf72ea5c53f35bfe59e1f418c098022849f
clementroume <clement.roume@hotmail.com>
2024-11-22 12:41:18 +0100

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	int n = 0;
+
+	while ((*s > 0 && *s <= 47) || (*s >= 58 && *s <= 127))
+	{
+		if (*s == '-')
+		{
+			sign *= -1;
+			s++;
+		}
+		else
+			s++;
+	}
+	while (*s >= '0' && *s <= '9')
+		n = (10 * n + *s++ - '0');
+	return (sign * n);
+}
