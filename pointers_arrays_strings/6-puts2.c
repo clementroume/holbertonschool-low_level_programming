7387ee22d34bc438f6ec05eb6f01955a9ad68f03
clementroume <clement.roume@hotmail.com>
2024-11-22 10:26:01 +0100

    Half the lies they tell about me aren't true

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: The string
+ */
+
+void puts2(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		str += 2;
+	}
+	_putchar('\n');
+}
