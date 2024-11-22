18c5b8efb3066bf487492adae5be1c88650e967f
clementroume <clement.roume@hotmail.com>
2024-11-22 09:58:11 +0100

    A good engineer thinks in reverse

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _strlen - function return the length of a string
+ * @s: The string
+ * Return: String length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * rev_string - function to revert a string
+ * @s: The string
+ */
+void rev_string(char *s)
+{
+	int first = -1;
+	int last = _strlen(s);
+	char temp;
+
+	while (++first < --last)
+	{
+		temp = s[first];
+		s[first] = s[last];
+		s[last] = temp;
+	}
+}
