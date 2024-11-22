90d9faf815fd67618b0ff4d54ab6dff80cbeee50
clementroume <clement.roume@hotmail.com>
2024-11-22 11:14:05 +0100

    Winning is only half of it

---
+++ pointers_arrays_strings/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+	char *str2;
+
+	str = "0123456789";
+	puts_half(str);
+	str2 = "012345678";
+	puts_half(str2);
+	return (0);
+}
---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _strlen - function return the length of a string
+ * @s: The string
+ * Return: String length
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * puts_half - function that prints half of a string
+ * @str: The string
+ */
+
+void puts_half(char *str)
+{
+	int len = _strlen(str) - 1;
+	int i;
+
+	if (len % 2 == 0)
+	{
+		i = len / 2 + 1;
+	}
+	else
+	{
+		i = (len - 1) / 2 + 1;
+	}
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i++]);
+	}
+	_putchar('\n');
+}
